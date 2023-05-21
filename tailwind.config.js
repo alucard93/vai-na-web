6868ad9c522a6f827d9fe38edee5a0f3ce9a926b
Marcus Vinicius Nascimento Ferreira <m.vinicius93.brasilmortal@gmail.com>
2023-05-21 14:02:52 -0300

    feat: desafio01
    - confecção do desafio01

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
